8ad5c9ff182cae6c6e80d91cbf1d4d4452f99dfa
catalunha <catalunha.mj@gmail.com>
2023-07-10 20:09:47 -0300

    rev

--- lib/event/event_action_01.dart
+++ lib/event/event_action_01.dart
@@ -8,20 +8,79 @@ import 'entity/event_entity.dart';
 
 Future<void> eventAction01() async {
   print('+++ eventAction01...');
+  try {
+    final QueryBuilder<ParseObject> queryAttendance =
+        QueryBuilder<ParseObject>(ParseObject(AttendanceEntity.className));
+    // final start = DateTime(2023, 06, 25);
+    // final end = DateTime(2023, 06, 27);
 
-  final QueryBuilder<ParseObject> queryAttendance =
-      QueryBuilder<ParseObject>(ParseObject(AttendanceEntity.className));
-  queryAttendance.whereEqualTo(
-      AttendanceEntity.professional,
-      (ParseObject(UserProfileEntity.className)..objectId = 'hE0Ng2FSwQ')
-          .toPointer());
+    // queryAttendance.whereGreaterThanOrEqualsTo(
+    //     AttendanceEntity.authorizationDateCreated,
+    //     DateTime(start.year, start.month, start.day));
+    // queryAttendance.whereLessThanOrEqualTo(
+    //     AttendanceEntity.authorizationDateCreated,
+    //     DateTime(end.year, end.month, end.day, 23, 59));
 
-  final list = await AttendanceB4a().list(
-    queryAttendance,
-    cols: {
-      "${AttendanceEntity.className}.cols": [AttendanceEntity.id]
-    },
-  );
+    queryAttendance.whereEqualTo(
+        AttendanceEntity.professional,
+        (ParseObject(UserProfileEntity.className)..objectId = 'hE0Ng2FSwQ')
+            .toPointer());
+
+    final list = await AttendanceB4a().list(
+      queryAttendance,
+      cols: {
+        "${AttendanceEntity.className}.cols": [AttendanceEntity.id]
+      },
+    );
+    for (var element in list) {
+      print('${element.id}');
+    }
+
+    final QueryBuilder<ParseObject> queryEvent =
+        QueryBuilder<ParseObject>(ParseObject(EventEntity.className));
+    List<QueryBuilder<ParseObject>> listQueries = [];
+
+    for (var element in list) {
+      final QueryBuilder<ParseObject> queryTemp =
+          QueryBuilder<ParseObject>(ParseObject(EventEntity.className));
+      // final start = DateTime(2023, 06, 25);
+      // final end = DateTime(2023, 06, 27);
+
+      // queryTemp.whereGreaterThanOrEqualsTo(
+      //     EventEntity.day, DateTime(start.year, start.month, start.day));
+      // queryTemp.whereLessThanOrEqualTo(
+      //     EventEntity.day, DateTime(end.year, end.month, end.day, 23, 59));
+
+      queryTemp.whereEqualTo(
+        EventEntity.attendances,
+        (ParseObject(AttendanceEntity.className)..objectId = element.id)
+            .toPointer(),
+      );
+
+      listQueries.add(queryTemp);
+    }
+    QueryBuilder<ParseObject> eventsOr = QueryBuilder.or(
+      ParseObject(EventEntity.className),
+      listQueries,
+    );
+    final start = DateTime(2023, 06, 25);
+    final end = DateTime(2023, 06, 27);
+
+    eventsOr.whereGreaterThanOrEqualsTo(
+        EventEntity.day, DateTime(start.year, start.month, start.day));
+    eventsOr.whereLessThanOrEqualTo(
+        EventEntity.day, DateTime(end.year, end.month, end.day, 23, 59));
+    eventsOr.orderByDescending('updatedAt');
+
+    final list2 = await EventB4a().list(
+      eventsOr,
+    );
+
+    for (var element in list2) {
+      print('${element.id}');
+    }
+
+    /*
 
   final QueryBuilder<ParseObject> queryByDate =
       QueryBuilder<ParseObject>(ParseObject(EventEntity.className));
@@ -32,21 +91,12 @@ Future<void> eventAction01() async {
       EventEntity.day, DateTime(start.year, start.month, start.day));
   queryByDate.whereLessThanOrEqualTo(
       EventEntity.day, DateTime(end.year, end.month, end.day, 23, 59));
-  // query.whereEqualTo(
-  //     EventEntity.attendances,
-  //     (ParseObject(AttendanceEntity.className)..objectId = 'f4KXlIwQYq')
-  //         .toPointer());
+
   List<QueryBuilder<ParseObject>> listQueries = [];
   for (var element in list) {
     final QueryBuilder<ParseObject> queryTemp =
         QueryBuilder<ParseObject>(ParseObject(EventEntity.className));
-    // final start = DateTime(2023, 06, 25);
-    // final end = DateTime(2023, 06, 27);
 
-    // queryTemp.whereGreaterThanOrEqualsTo(
-    //     EventEntity.day, DateTime(start.year, start.month, start.day));
-    // queryTemp.whereLessThanOrEqualTo(
-    //     EventEntity.day, DateTime(end.year, end.month, end.day, 23, 59));
     queryTemp.whereEqualTo(
         EventEntity.attendances,
         (ParseObject(AttendanceEntity.className)..objectId = element.id)
@@ -64,6 +114,38 @@ Future<void> eventAction01() async {
     eventsOr,
   ]);
 
+
+    final list2 = await EventB4a().list(
+      eventsAnd,
+    );
+
+    int index = 1;
+    for (var element in list2) {
+      print('${index++}: ${element.id}');
+    }
+    */
+  } catch (error, stackTrace) {
+    print(error);
+    print(stackTrace);
+  }
+  print('--- eventAction01...');
+}
+
+
+/*
+  // query.whereEqualTo(
+  //     EventEntity.attendances,
+  //     (ParseObject(AttendanceEntity.className)..objectId = 'f4KXlIwQYq')
+  //         .toPointer());
+
+    // final start = DateTime(2023, 06, 25);
+    // final end = DateTime(2023, 06, 27);
+
+    // queryTemp.whereGreaterThanOrEqualsTo(
+    //     EventEntity.day, DateTime(start.year, start.month, start.day));
+    // queryTemp.whereLessThanOrEqualTo(
+    //     EventEntity.day, DateTime(end.year, end.month, end.day, 23, 59));
+
 /*
   final QueryBuilder<ParseObject> queryAttendance =
       QueryBuilder<ParseObject>(ParseObject(AttendanceEntity.className));
@@ -72,6 +154,7 @@ Future<void> eventAction01() async {
       (ParseObject(UserProfileEntity.className)..objectId = 'hE0Ng2FSwQ')
           .toPointer());
 */
+
   // final QueryBuilder<ParseObject> queryAttendance =
   //     QueryBuilder<ParseObject>(ParseObject(AttendanceEntity.className));
   // queryAttendance.whereEqualTo(AttendanceEntity.id, 'f4KXlIwQYq');
@@ -83,26 +166,8 @@ Future<void> eventAction01() async {
   //     (ParseObject(AttendanceEntity.className)..objectId = 'f4KXlIwQYq')
   //         .toPointer());
 
-/*
-
 query.whereEqualTo(
 EventEntity.attendances,
 (ParseObject(AttendanceEntity.className)..objectId = 'f4KXlIwQYq')
     .toPointer());
-    */
-
-  try {
-    final list2 = await EventB4a().list(
-      eventsAnd,
-    );
-
-    int index = 1;
-    for (var element in list2) {
-      print('${index++}: ${element.id}');
-    }
-  } catch (error, stackTrace) {
-    print(error);
-    print(stackTrace);
-  }
-  print('--- eventAction01...');
-}
+*/
